b83a51594dd6343ed85d2d492ee01bd778d7b38e
Samuel Lowe <samlowe106@gmail.com>
2024-05-09 01:32:43 -0400

    Added test and src file templates

---
+++ test/test.py
@@ -0,0 +1,8 @@
+# Remove these flake8 ignore comments when you're ready to use this file
+# flake8: noqa
+import unittest
+from unittest.mock import Mock, patch
+
+
+class TestSomething(unittest.TestCase):
+    pass
