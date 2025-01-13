af6acd66986e34c6726695cd7261c11f4f1b7a0e
Samuel Lowe <samlowe106@gmail.com>
2025-01-12 19:23:01 -0500

    updated setup, setup now adds poetry packages

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
-curl -sSL https://install.python-poetry.org | python3 -
+sudo apt install pipx
+pipx install poetry
 
+poetry init
+poetry add pre-commit
 poetry install
 
-# This installs and updates pre-commit globally
-python3 -m pip install --upgrade pip
-pip install --upgrade pre-commit
-
-# To install pre-commit locally, comment out the above and instead run
-# poetry add pre-commit
-# poetry shell
-
+pre-commit autoupdate
 pre-commit install
