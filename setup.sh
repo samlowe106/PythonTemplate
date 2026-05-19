581faa8e12fe289e2e68bff8f1f868a60df64c4e
Samuel Lowe <samlowe106@gmail.com>
2026-05-19 02:24:16 -0400

    setup now exits immediately if OS isn't linux or darwin or if any command has a nonzero return code, and installs uv

--- setup.sh
+++ setup.sh
@@ -1,8 +1,24 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-# install uv using one of the following:
-# curl -LsSf https://astral.sh/uv/install.sh | sh
-# winget install --id=astral-sh.uv  -e
+set -euo pipefail
+
+OS="$(uname -s)"
+
+if [[ ! "$OS" == "Linux" && ! "$OS" == "Darwin" ]]; then
+    echo "Unsupported OS: $OS" >&2
+    exit 1
+fi
+
+if ! command -v uv >/dev/null 2>&1; then
+    echo "uv not found. Installing..."
+
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+
+    # Add uv to PATH for current shell session
+    export PATH="$HOME/.local/bin:$PATH"
+else
+    echo "verified $(uv --version)"
+fi
 
 uv init
 uv add pre-commit pytest python-dotenv
